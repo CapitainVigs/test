42d05aa72f1afe27fd3f67df681bf2d51e768177
CapitainVigs <vitoireofgod@gmail.com>
2020-03-05 16:21:30 +0100

    Components Part 1

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
      <div className="App">
@@ -10,7 +11,9 @@ function App() {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
+
   );
 }
 
